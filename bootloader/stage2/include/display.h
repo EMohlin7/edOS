4f82ab4c0f324d28466324d887e501e96f0eb785
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-05-03 22:04:55 +0200

    Added single include protection

--- bootloader/stage2/include/display.h
+++ bootloader/stage2/include/display.h
@@ -1,4 +1,7 @@
 #include <stdint.h>
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
 #define WHITE_BLACK_CHAR 0x0F
 
 void printChar(char character, uint8_t color);
@@ -7,3 +10,5 @@ void print(char character);
 void setCursorPos(uint64_t pos);
 
 void clearScreen();
+
+#endif //DISPLAY_H
