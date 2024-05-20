2aa34e356775ebf641c392d5dfdefda961581460
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-05-20 23:58:39 +0200

    Added ability to use recusive mapping

--- bootloader/stage2/source/c/pageTable.c
+++ bootloader/stage2/source/c/pageTable.c
@@ -2,6 +2,8 @@
 #define PAGE_SIZE 0x1000
 #define VID_MEM (short*)0xB8000
 
+//This code is compiled as 32 bit code because it is used to setup the page tables before entering long mode
+
 /// @brief Setup the initial page tables
 /// @param PML4 The physical address to place PML4 at. The rest of the tables will be placed directly following this address.
 void setupPageTable(uint32_t PML4){
@@ -17,12 +19,16 @@ void setupPageTable(uint32_t PML4){
 
     //Make the first entry of the tables point to the next table
     for(int i = 0; i < 3; ++i){
-        uint32_t* currTable = (uint32_t*)(tables + i*PAGE_SIZE);
+        uint64_t* currTable = (uint64_t*)(tables + i*PAGE_SIZE);
         *currTable = (uint32_t)currTable + PAGE_SIZE + 3;     //Add 3 to set P and R/W bits to 1
     }
 
+    //Point last entry of PML4 to itself to allow for recursive mapping
+    uint64_t* tmp = (uint64_t*)PML4;
+    tmp[511] = PML4 + 3;
 
-    //Map all of the entries in the pt to physical memory. First two MiB 0x0 - 0x200000
+
+    //Map all of the entries in the pt to physical memory. First two MiB 0x0 - 0x200000-1
     uint64_t* pt = (uint64_t*)PT;
     for(int i = 0; i < 512; ++i){
         pt[i] = PAGE_SIZE*i + 3;
