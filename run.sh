16792ae5e3c40eee302841a9ca4f0d517b923403
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-07-28 11:40:52 +0200

    Added kvm flag

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-qemu-system-x86_64 -drive file=edOS.img,format=raw,index=0,media=disk -no-shutdown -no-reboot -d int,cpu_reset
+qemu-system-x86_64 -enable-kvm -drive file=edOS.img,format=raw,index=0,media=disk -no-shutdown -no-reboot -d int,cpu_reset
