aaaa73aac6c48ffd2c785ff2aa76a55023337658
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-08-12 12:02:53 +0200

    Properly save registers in interrupts

--- bootloader/stage2/source/asm/idt.s
+++ bootloader/stage2/source/asm/idt.s
@@ -2,18 +2,58 @@
 
 extern defaultISR;(uint8_t vector, uint64_t error)
 
+%macro ISR_SAVE_REGS 0
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endmacro
+
+%macro ISR_RESTORE_REGS 0
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+%endmacro
+
 %macro isrEntry 1
 isr_%1:
+    ISR_SAVE_REGS
     mov rdi, %1
     mov rsi, 0
     call defaultISR
+    ISR_RESTORE_REGS
     iretq
 %endmacro
 %macro isrEntryErrCode 1
 isr_%1:
+    ISR_SAVE_REGS
     mov rdi, %1
     pop rsi
     call defaultISR
+    ISR_RESTORE_REGS
     iretq
 %endmacro
 
