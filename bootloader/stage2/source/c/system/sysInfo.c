dc01ae1567a8fd1ab0ab0f8aa672beae25aead2f
Edvin Mohlin <edvin.mohlin@gmail.com>
2024-08-12 12:25:48 +0200

    Updated printSysInfo function

--- bootloader/stage2/source/c/system/sysInfo.c
+++ bootloader/stage2/source/c/system/sysInfo.c
@@ -3,8 +3,12 @@
 void printSysInfo(void){
     char vendor[13];
     vendor[12] = NULL;
-    uint32_t eax = 0;
+    uint32_t eax = 0, ebx, ecx, edx;
     cpuId(&eax, (uint32_t*)vendor, (uint32_t*)(vendor+4), (uint32_t*)(vendor+8));
     printf("CPU vendor: %s\n", vendor);
+
+    eax = 0;
+    cpuId(&eax, &ebx, &ecx, &edx);
+    printf("Highest cpuid: %#.2x\n", eax);
 }
 
